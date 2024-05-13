aed7175b78e10c50ecee527b036dd6bfb61a6b55
alexxShandsome <algilbertgomez@gmail.com>
2024-05-14 00:11:17 +0800

    nvim-tree: improve

--- nvim/lua/alex/plugins/nvim-tree.lua
+++ nvim/lua/alex/plugins/nvim-tree.lua
@@ -3,13 +3,51 @@ return {
 	config = function()
 		vim.opt.termguicolors = true
 
+		vim.keymap.set("n", "<leader>F", function() vim.cmd("NvimTreeToggle") end,
+			{ desc = "NvimTreeToggle: NvimTree File Explorer" })
+		vim.keymap.set("n", "<F6>", function() vim.cmd("NvimTreeToggle") end,
+			{ desc = "NvimTreeToggle: NvimTree File Explorer" })
+
+		local HEIGHT_RATIO = 0.8 -- You can change this
+		local WIDTH_RATIO = 0.5 -- You can change this too
+
 		-- empty setup using defaults
 		require("nvim-tree").setup {
-			sort_by = "name",
+			sort = {
+				sorter = "name"
+			},
 			view = {
 				adaptive_size = false,
 				number = false,
 				relativenumber = false,
+				cursorline = true,
+				signcolumn = "auto",
+				float = {
+					enable = true,
+					open_win_config = function()
+						local screen_w = vim.opt.columns:get()
+						local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+						local window_w = screen_w * WIDTH_RATIO
+						local window_h = screen_h * HEIGHT_RATIO
+						local window_w_int = math.floor(window_w)
+						local window_h_int = math.floor(window_h)
+						local center_x = (screen_w - window_w) / 2
+						local center_y = ((vim.opt.lines:get() - window_h) / 2)
+							 - vim.opt.cmdheight:get()
+						return {
+							border = "rounded",
+							relative = "editor",
+							row = center_y,
+							col = center_x,
+							width = window_w_int,
+							height = window_h_int,
+						}
+					end
+				},
+				-- width = function()
+				-- 	return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+				-- end,
+				width = 30
 			},
 			renderer = {
 				add_trailing = true,
@@ -44,24 +82,13 @@ return {
 				custom = {},
 				exclude = {},
 			},
+			tab = {
+				sync = {
+					open = true,
+					close = true
+				}
+			}
 		}
-
-		local function open_nvim_tree(data)
-			-- buffer is a directory
-			local directory = vim.fn.isdirectory(data.file) == 1
-
-			if not directory then
-				return
-			end
-
-			-- change to the directory
-			vim.cmd.cd(data.file)
-
-			-- open the tree
-			require("nvim-tree.api").tree.open()
-		end
-
-		vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 	end,
 	dependencies = {
 		"nvim-tree/nvim-web-devicons"
