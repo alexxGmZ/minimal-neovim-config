3e247dcf86764179c4ec034d5d22aec53da289fd
alexxShandsome <algilbertgomez@gmail.com>
2024-05-14 00:10:58 +0800

    colorscheme: everforest main

--- nvim/lua/alex/plugins/colorscheme/everforest.lua
+++ nvim/lua/alex/plugins/colorscheme/everforest.lua
@@ -1,57 +1,51 @@
 return {
 	"neanias/everforest-nvim",
-	event = "CmdlineChanged",
+	-- event = "CmdlineChanged",
+	priority = 1000,
 	config = function()
+		vim.cmd("hi @markup.italic gui=italic")
+
 		require("everforest").setup({
-			---Controls the "hardness" of the background. Options are "soft", "medium" or "hard".
-			---Default is "medium".
-			background = "medium",
-			---How much of the background should be transparent. 2 will have more UI
-			---components be transparent (e.g. status line background)
-			transparent_background_level = 0,
-			---Whether italics should be used for keywords and more.
+			background = "hard",
+			transparent_background_level = 1,
 			italics = true,
-			---Disable italic fonts for comments. Comments are in italics by default, set
-			---this to `true` to make them _not_ italic!
 			disable_italic_comments = false,
-			---By default, the colour of the sign column background is the same as the as normal text
-			---background, but you can use a grey background by setting this to `"grey"`.
 			sign_column_background = "none",
-			---The contrast of line numbers, indent lines, etc. Options are `"high"` or
-			---`"low"` (default).
 			ui_contrast = "high",
-			---Dim inactive windows. Only works in Neovim. Can look a bit weird with Telescope.
-			---
-			---When this option is used in conjunction with show_eob set to `false`, the
-			---end of the buffer will only be hidden inside the active window. Inside
-			---inactive windows, the end of buffer filler characters will be visible in
-			---dimmed symbols. This is due to the way Vim and Neovim handle `EndOfBuffer`.
-			dim_inactive_windows = true,
-			---Some plugins support highlighting error/warning/info/hint texts, by
-			---default these texts are only underlined, but you can use this option to
-			---also highlight the background of them.
+			dim_inactive_windows = false,
 			diagnostic_text_highlight = false,
-			---Which colour the diagnostic text should be. Options are `"grey"` or `"coloured"` (default)
 			diagnostic_virtual_text = "coloured",
-			---Some plugins support highlighting error/warning/info/hint lines, but this
-			---feature is disabled by default in this colour scheme.
 			diagnostic_line_highlight = false,
-			---By default, this color scheme won't colour the foreground of |spell|, instead
-			---colored under curls will be used. If you also want to colour the foreground,
-			---set this option to `true`.
 			spell_foreground = false,
-			---Whether to show the EndOfBuffer highlight.
 			show_eob = true,
-			---You can override specific highlights to use other groups or a hex colour.
-			---This function will be called with the highlights and colour palette tables.
-			---@param highlight_groups Highlights
-			---@param palette Palette
-			on_highlights = function(highlight_groups, palette) end,
-			---You can override colours in the palette to use different hex colours.
-			---This function will be called once the base and background colours have
-			---been mixed on the palette.
+			float_style = "dim",
+			---@param hl
+			---@param palette
+			on_highlights = function(hl, palette)
+				hl.TSBoolean = { fg = palette.purple, bold = true }
+				hl.TSFloat = { fg = palette.purple, bold = true }
+				hl.TSNumber = { fg = palette.purple, bold = true }
+				hl.TSFunction = { fg = palette.orange, bold = true }
+				hl.TSFunctionCall = { fg = palette.orange, bold = true }
+				hl.TSKeyword = { fg = palette.red, italic = true }
+				hl.TSParameter = { fg = palette.red, bold = true }
+				hl.TSString = { fg = palette.green, bold = true }
+				hl.TSConditional = { fg = palette.red, italic = true }
+				hl.TSVariable = { fg = palette.none, bold = true }
+
+				hl.CurrentWord = { fg = palette.none, underline = true }
+				hl.String = { fg = palette.green, bold = true }
+				hl.Keyword = { fg = palette.red, italic = true }
+				hl.MatchParen = { fg = palette.orange, bold = true, underline = true }
+				hl.Constant = { fg = palette.orange, bold = true }
+
+				hl.BufferlineSeparator = { fg = palette.bg0 }
+				hl.BufferlineSeparatorSelected = { fg = palette.bg0 }
+				-- hl.BufferlineTab = { fg = palette.bg_dim }
+			end,
 			---@param palette Palette
 			colours_override = function(palette) end,
 		})
+		vim.cmd.colorscheme "everforest"
 	end
 }
