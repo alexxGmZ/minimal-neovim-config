5423dd8abe76c245a82dc87ad52c79b75d804740
alexxShandsome <algilbertgomez@gmail.com>
2024-06-07 18:07:29 +0800

    fzf-lua: previwer config

--- nvim/lua/alex/plugins/fzf-lua.lua
+++ nvim/lua/alex/plugins/fzf-lua.lua
@@ -3,5 +3,21 @@ return {
 	cmd = "FzfLua",
 	dependencies = {
 		"junegunn/fzf",
-	}
+	},
+	config = function()
+		require("fzf-lua").setup {
+			winopts = {},
+			previewers = {
+				builtin = {
+					syntax = true,
+					treesitter = { enable = false },
+					extensions = {
+						["svg"] = "chafa",
+						["png"] = "chafa",
+						["jpg"] = "chafa",
+					}
+				}
+			}
+		}
+	end
 }
