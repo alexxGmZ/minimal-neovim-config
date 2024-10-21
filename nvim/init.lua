eb8a49cabb0488c1507e6417d42d78379987e095
alexxShandsome <algilbertgomez@gmail.com>
2024-10-21 18:29:19 +0800

    remove old lazy init

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,30 +28,6 @@ require("lazy").setup({
    ui = { border = "rounded" },
    change_detection = { enabled = false }
 })
--- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- if not vim.loop.fs_stat(lazypath) then
--- 	vim.fn.system({
--- 		"git",
--- 		"clone",
--- 		"--filter=blob:none",
--- 		"https://github.com/folke/lazy.nvim.git",
--- 		"--branch=stable", -- latest stable release
--- 		lazypath,
--- 	})
--- end
--- vim.opt.rtp:prepend(lazypath)
---
--- require("lazy").setup(
--- 	-- ./lua/plugins.lua file
--- 	require("plugins"),
---
--- 	{
--- 		ui = {
--- 			border = "rounded",
--- 		},
---
--- 	}
--- )
 
 -- neovide config
 if vim.g.neovide then
