f0bce25fbe5923d6f3942809f4b60f4476bbc219
alexxShandsome <algilbertgomez@gmail.com>
2024-10-22 13:36:08 +0800

    fzf-lua: update config

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -1,23 +1,40 @@
 return {
-	"ibhagwan/fzf-lua",
-	cmd = "FzfLua",
-	dependencies = {
-		"junegunn/fzf",
-	},
-	config = function()
-		require("fzf-lua").setup {
-			winopts = {},
-			previewers = {
-				builtin = {
-					syntax = true,
-					treesitter = { enable = false },
-					extensions = {
-						["svg"] = "chafa",
-						["png"] = "chafa",
-						["jpg"] = "chafa",
-					}
-				}
-			}
-		}
-	end
+   "ibhagwan/fzf-lua",
+   cmd = "FzfLua",
+   dependencies = {
+      "junegunn/fzf",
+   },
+   keys = {
+      { "<C-p>",      "<cmd>FzfLua files<CR>",     desc = "FzfLua: FzfLua files" },
+      { "<leader>lg", "<cmd>FzfLua live_grep<CR>", desc = "FzfLua: FzfLua live grep" }
+   },
+   config = function()
+      local actions = require("fzf-lua.actions")
+      require("fzf-lua").setup {
+         winopts = {},
+         previewers = {
+            builtin = {
+               syntax = false,
+               treesitter = { enable = false },
+               extensions = {
+                  ["svg"] = "chafa",
+                  ["png"] = "chafa",
+                  ["jpg"] = "chafa",
+               }
+            }
+         },
+         keymap = {
+            fzf = {
+               true,
+               ["ctrl-a"] = "toggle-all",
+            }
+         },
+         actions = {
+            files = {
+               true,
+               ["ctrl-q"] = actions.file_sel_to_qf,
+            }
+         }
+      }
+   end
 }
