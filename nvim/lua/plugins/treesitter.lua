b5803c491fb9cc530fd0d8e5813ff9835fb4cc4a
alexxShandsome <algilbertgomez@gmail.com>
2024-10-22 13:32:00 +0800

    add treesitter-context and treesj

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,30 +1,64 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local treesitter = require('nvim-treesitter.configs')
-		treesitter.setup {
-			ensure_installed = {
-				"bash",
-				"c",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"query",
-				"vim",
-				"vimdoc"
-			},
-			sync_install = false,
-			auto_install = true,
-			ignore_install = {},
-			highlight = {
-				enable = false,
-				disable = { "lua", "javascript", "go" },
-				additional_vim_regex_highlighting = true,
-			},
-			incremental_selection = { enable = false },
-			indent = { enable = false }
-		}
-	end,
+   {
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+      config = function()
+         local treesitter = require('nvim-treesitter.configs')
+         treesitter.setup {
+            ensure_installed = {
+               "bash",
+               "c",
+               "lua",
+               "markdown",
+               "markdown_inline",
+               "python",
+               "query",
+               "vim",
+               "vimdoc"
+            },
+            sync_install = false,
+            auto_install = true,
+            ignore_install = {},
+            highlight = {
+               enable = false,
+               disable = { "lua", "javascript", "go" },
+               additional_vim_regex_highlighting = true,
+            },
+            incremental_selection = { enable = false },
+            indent = { enable = false }
+         }
+      end
+   },
+   {
+      "nvim-treesitter/nvim-treesitter-context",
+      event = "VeryLazy",
+      dependencies = { "nvim-treesitter/nvim-treesitter" },
+      config = function()
+         require "treesitter-context".setup {
+            enable = true,
+            max_lines = 0,
+            min_window_height = 0,
+            line_numbers = true,
+            multiline_threshold = 20,
+            trim_scope = "outer",
+            mode = "cursor",
+            separator = nil,
+            zindex = 20,
+            on_attach = nil,
+         }
+      end
+   },
+   {
+      "Wansmer/treesj",
+      keys = {
+         { "<leader>m", "<cmd>TSJToggle<cr>", desc = "TreeSJ: Toggle" }
+      },
+      dependencies = { "nvim-treesitter/nvim-treesitter" },
+      config = function()
+         require("treesj").setup({
+            use_default_keymaps = true,
+            max_join_length = 200,
+         })
+      end
+   }
 }
--- nvim/lua/plugins/vim-illuminate.lua
+++ nvim/lua/plugins/vim-illuminate.lua
@@ -1,35 +1,24 @@
-local function illuminate_highlights()
-	vim.cmd [[
-		hi IlluminatedWordText guibg=#4e4e4e gui=NONE
-		hi IlluminatedWordRead guibg=#4e4e4e gui=NONE
-		hi IlluminatedWordWrite guibg=#4e4e4e gui=NONE
-	]]
-end
-
 return {
-	"RRethy/vim-illuminate",
-	event = "VeryLazy",
-	config = function()
-		require("illuminate").configure({
-			providers = { 'lsp', 'treesitter', 'regex' },
-			modes_allowlist = { "n" },
-			filetypes_denylist = {
-				"help",
-				"text",
-				"man",
-				"checkhealth",
-				"NvimTree",
-				"Outline"
-			},
-			min_count_to_highlight = 2,
-		})
-
-		illuminate_highlights()
-
-		vim.api.nvim_create_autocmd("ColorScheme", {
-			pattern = "*",
-			group = "HANDSOME",
-			callback = illuminate_highlights,
-		})
-	end
+   "RRethy/vim-illuminate",
+   cmd = {
+      "IlluminateToggle",
+      "IlluminateToggleBuf",
+      "IlluminateResume",
+      "IlluminateResumeBuf",
+   },
+   config = function()
+      require("illuminate").configure({
+         providers = { "lsp", "treesitter", "regex" },
+         modes_allowlist = { "n" },
+         filetypes_denylist = {
+            "help",
+            "text",
+            "man",
+            "checkhealth",
+            "NvimTree",
+            "Outline"
+         },
+         min_count_to_highlight = 2,
+      })
+   end
 }
