4760220b6f902bb3d507f0ce3b0951d10fb8fb79
alexxGmZ <algilbertgomez@gmail.com>
2025-06-29 19:24:16 +0800

    git: git plugins unified

---
+++ nvim/lua/plugins/git.lua
@@ -0,0 +1,31 @@
+return {
+   {
+      "lewis6991/gitsigns.nvim",
+      event = "VeryLazy",
+      config = function()
+         require("gitsigns").setup {
+            current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+            current_line_blame_opts = {
+               virt_text = true,
+               virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+               delay = 500,
+               ignore_whitespace = false,
+            },
+            current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d>, <abbrev_sha> - \"<summary>\"",
+         }
+      end
+   },
+   {
+      "tpope/vim-fugitive",
+      cmd = { "Git", "G" }
+   },
+   {
+      "akinsho/git-conflict.nvim",
+      cmd = "GitConflictListQf",
+      version = "*",
+      config = function()
+         require("git-conflict").setup()
+      end
+   }
+}
+
