15347cbd460fb93423dd8f0b39dd0bda40acae40
alexxGmZ <algilbertgomez@gmail.com>
2025-06-29 19:23:12 +0800

    eyeliner: update

--- nvim/lua/plugins/eyeliner.lua
+++ nvim/lua/plugins/eyeliner.lua
@@ -1,9 +1,20 @@
 return {
    "jinh0/eyeliner.nvim",
+   keys = {
+      { "f" },
+      { "F" },
+      { "t" },
+      { "T" },
+      { "f", mode = "v" },
+      { "F", mode = "v" },
+      { "t", mode = "v" },
+      { "T", mode = "v" },
+   },
+   -- event = "VeryLazy",
    config = function ()
       require("eyeliner").setup({
          highlight_on_key = true,
-         dim = true,
+         dim = false,
          default_keymaps = true
       })
    end
