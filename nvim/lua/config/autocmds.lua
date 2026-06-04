a6d5cb0abf522d0cc140d8c6870e167828b98f1a
alexxGmZ <algilbertgomez@gmail.com>
2026-06-04 13:46:07 +0800

    autocmds: typst auto compile

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -37,3 +37,35 @@ vim.api.nvim_create_autocmd({ "CmdlineLeave", "UIEnter" }, {
 		vim.opt.cmdheight = 0
 	end
 })
+
+create_autocmd("BufWritePost", {
+   pattern = "*.typ",
+   group = user_augroup,
+   callback = function()
+      local filename = vim.api.nvim_buf_get_name(0)
+      local notify = function(message, log_level)
+         vim.notify(message, log_level, { title = "Typst" })
+      end
+
+      if vim.fn.executable('typst') == 0 then
+         notify("typst executable not found", "ERROR")
+         return
+      end
+
+      vim.fn.jobstart({ "typst", "compile", filename }, {
+         detach = true,
+         stderr_buffered = true,
+         on_stderr = function(_, data)
+            local err_msg = table.concat(data, "\n")
+            if err_msg ~= "" then
+               notify(err_msg, "ERROR")
+            end
+         end,
+         on_exit = function(_, code)
+            if code == 0 then
+               notify("Compiled succesfully.")
+            end
+         end
+      })
+   end
+})
