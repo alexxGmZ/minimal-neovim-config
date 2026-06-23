65e8abce433826b651003b53cad4c851f74059ad
alexxGmZ <algilbertgomez@gmail.com>
2026-06-23 18:26:53 +0800

    keymaps: add dict keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -29,3 +29,56 @@ map.set("n", "<leader>-", "<C-w>3-", { desc = "Pane: Decrease current pane heigh
 map.set("n", "<leader>=", "<C-w>3+", { desc = "Pane: Increate current pane height" })
 map.set("n", "<leader>.", "<C-w>3>", { desc = "Pane: Increase current pane width" })
 map.set("n", "<leader>,", "<C-w>3<", { desc = "Pane: Decrease current pane width" })
+
+local function define_word()
+   local word = vim.fn.expand("<cword>")
+   local buffer = vim.api.nvim_create_buf(false, true)
+   local window_height = 25
+   local lines = nil
+   local buffer_line_count = 0
+
+   if vim.fn.executable('dict') == 0 then
+      vim.notify("dict executable not found", "ERROR")
+      return
+   end
+
+   local command = vim.system({ "dict", word }, { text = true }):wait()
+
+   if command.code ~= 0 then
+      lines = vim.split(command.stderr, "\n", { plain = true })
+   else
+      lines = vim.split(command.stdout, "\n", { plain = true })
+   end
+
+   vim.api.nvim_buf_set_lines(buffer, 0, -1, false, lines)
+   buffer_line_count = vim.api.nvim_buf_line_count(buffer)
+   vim.bo[buffer].modifiable = false
+   vim.bo[buffer].readonly = true
+
+   if buffer_line_count < window_height then
+      window_height = buffer_line_count
+   end
+
+   local window = vim.api.nvim_open_win(buffer, true, {
+      relative = "cursor",
+      bufpos = { 0, 0 },
+      border = "single",
+      width = 85,
+      height = window_height,
+      style = "minimal",
+      title = word
+   })
+
+   vim.api.nvim_create_autocmd("WinLeave", {
+      buf = buffer,
+      once = true,
+      callback = function()
+         vim.api.nvim_win_close(window, true)
+         vim.bo[buffer].bufhidden = "wipe"
+      end
+   })
+
+   print("Dictionary", word)
+end
+
+map.set("n", "<leader>dc", define_word, { desc = "Dictionary" })
